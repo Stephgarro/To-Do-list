93182725e02ce97b6d17b8c01ed0157a1e2f5ae1
Stephgarro <109922003+Stephgarro@users.noreply.github.com>
2023-11-08 12:11:28 -0600

    feat( react): some changes

--- todo-list-app/src/components/Task.js
+++ todo-list-app/src/components/Task.js
@@ -1,23 +1,4 @@
 
-
-// import React from 'react';
-
-// function Task({ text, onComplete, showCompleteButton }) {
-//   return (
-//     <div className="task">
-//       <span>{text}</span>
-//       <button
-//         className={`task-complete-button ${showCompleteButton ? '' : 'Completed'}`}
-//         onClick={onComplete}
-//       >
-//         Complete
-//       </button>
-//     </div>
-//   );
-// }
-
-// export default Task;
-
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faTrash } from '@fortawesome/free-solid-svg-icons';
--- todo-list-app/src/components/TaskList.js
+++ todo-list-app/src/components/TaskList.js
@@ -1,57 +1,5 @@
 
 
-// import React, { useState } from 'react';
-// import Task from './Task';
-
-
-// function TaskList({ title, tasks, onTaskAdd, onTaskComplete }) {
-//   const [newTaskText, setNewTaskText] = useState('');
-
-//   const handleAddClick = () => {
-//     if (newTaskText) {
-//       onTaskAdd(newTaskText);
-//       setNewTaskText('');
-//     }
-//   };
-
-//   const isCompleted = title === 'Completed'; // Mueve la definición de isCompleted aquí
-
-//   return (
-//     <div className="task-list">
-//       <h2>{title}</h2>
-//       <div className="task-list-content">
-//         <div className="task-list-items">
-//           {tasks.map((task, index) => {
-//             const showCompleteButton = !isCompleted && !task.isCompleted; // Muestra el botón solo si no está en "completed" y no se ha completado
-
-//             return (
-//               <Task
-//                 key={index}
-//                 text={task.text}
-//                 onComplete={() => onTaskComplete(index)}
-//                 showCompleteButton={showCompleteButton}
-//               />
-//             );
-//           })}
-//         </div>
-//         {!isCompleted && ( 
-//           <div className="task-list-add">
-//             <input
-//               type="text"
-//               value={newTaskText}
-//               onChange={(e) => setNewTaskText(e.target.value)}
-//               placeholder="New task"
-//             />
-//             <button className="add-btn" onClick={handleAddClick}>Add task</button>
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default TaskList;
-
 import React, { useState } from 'react';
 import Task from './Task';
 
@@ -80,7 +28,7 @@ function TaskList({ title, tasks, onTaskAdd, onTaskComplete, onTaskRemove }) {
                 key={index}
                 text={task.text}
                 onComplete={() => onTaskComplete(index)}
-                onRemove={() => onTaskRemove(index)}
+                onRemove={() => onTaskRemove(index, title)}  
                 showCompleteButton={showCompleteButton}
               />
             );
@@ -104,4 +52,11 @@ function TaskList({ title, tasks, onTaskAdd, onTaskComplete, onTaskRemove }) {
 }
 
 export default TaskList;
+
+
+
+
+
+
+
 
